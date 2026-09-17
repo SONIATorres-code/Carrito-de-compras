ad3f484986a27c4e0f91eb5c31e28f2aee73a923
Brandon <brandon14espmai@gmail.com>
2026-09-16 23:22:25 -0600

    Revert "Agrega reglas de descuento"
    This reverts commit 6ddd3db5b32897b062741837d2559d4564e53cc6.

--- descuentos.py
+++
@@ -1,9 +0,0 @@
-"""Reglas de promoción y cálculo de importes."""
-
-# Valores de descuento por tipo
-REGLAS_DESCUENTO = {
-    "ninguno": 0.0,
-    "porcentaje": 0.10,
-    "mayorista": 0.15,
-    "3x2": "paga dos y selecciona tres",
-}
