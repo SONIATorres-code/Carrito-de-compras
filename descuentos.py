629b620b76b957d68c3277b11eaade01e2ffb00d
Brandon <brandon14espmai@gmail.com>
2026-09-16 22:50:21 -0600

    Agrega reglas de descuento

---
+++ descuentos.py
@@ -0,0 +1,9 @@
+"""Reglas de promoción y cálculo de importes."""
+
+# Valores de descuento por tipo
+REGLAS_DESCUENTO = {
+    "ninguno": 0.0,
+    "porcentaje": 0.10,
+    "mayorista": 0.15,
+    "3x2": "paga dos y selecciona tres",
+}
