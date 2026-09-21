d0e6bd8e61c4194743391b58e8d48bca08bede06
Brandon <brandon14espmai@gmail.com>
2026-09-21 13:14:37 -0600

    Corrige coma faltante y llave de cierre en catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -5,7 +5,7 @@ def cargar_catalogo():
         "P002": {"nombre": "Capuchino",      "precio": 55.0, "stock": 18},
         "P003": {"nombre": "Pay de Limon",   "precio": 35.0, "stock": 10},
         "P004": {"nombre": "Te Verde",       "precio": 40.0, "stock": 12},
-        "P005": {"nombre": "Sandwich Jamon", "precio": 38.0, "stock": 8}
+        "P005": {"nombre": "Sandwich Jamon", "precio": 38.0, "stock": 8},
         "P006": {"nombre": "Muffin Vainilla", "precio": 28.0, "stock": 10},
     }
     return catalogo
